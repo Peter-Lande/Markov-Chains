c48c8950fa4efdd287a2866cc76f87e42b479d2e
Peter <peter.lande@colorado.edu>
2022-07-12 12:20:23 -0600

    Reformatted into object based program
    Converted code to object based. Additionally, added *.txt and *.json to gitignore.

--- markov_chains.py
+++ markov_chains.py
@@ -4,6 +4,64 @@ import argparse
 import sys
 
 
+class MarkovSystem:
+    def __init__(self, state_length: int = 1, save_filename: str = 'markov_system.json') -> None:
+        self.state_dictionary: dict[str, dict[str, int]] = {}
+        self.state_length: int = state_length
+        self.save_file = save_filename
+
+    def __populate_system(self, word_list: list[str]) -> None:
+        k_length_states = [' '.join(word_list[i:i+self.state_length])
+                           for i, _ in enumerate(word_list[:-self.state_length])]
+        if len(self.state_dictionary) == 0:
+            self.state_dictionary = {k_state: {} for k_state in set(k_length_states)}
+        else:
+            new_markov_dictionary = {k_state: {} for k_state in set(k_length_states)}
+            self.state_dictionary = new_markov_dictionary | self.state_dictionary
+        for index, k_length_state in enumerate(k_length_states[:-1]):
+            if k_length_states[index+1] in self.state_dictionary[k_length_state]:
+                self.state_dictionary[k_length_state][k_length_states[index+1].split()[-1]] += 1
+            else:
+                self.state_dictionary[k_length_state][k_length_states[index+1].split()[-1]] = 1
+
+    def load_text(self, filename: str) -> None:
+        self.__populate_system(clean_input_text(filename))
+
+    def save(self) -> None:
+        with open(self.save_file, 'w') as f:
+            json.dump(self.state_dictionary, f)
+
+    def load(self) -> None:
+        with open(self.save_file, 'r') as f:
+            self.state_dictionary = json.load(f)
+
+    def sample_next_state(self, current_state: str):
+        next_state_vector_weights = [weight for weight in self.state_dictionary[current_state].values()]
+        next_state_vector = [key for key in self.state_dictionary[current_state].keys()]
+        return random.choices(next_state_vector, next_state_vector_weights)[0]
+
+    def stochastic_chain(self, seed: str = None, chain_length: int = 15) -> str:
+        if seed:
+            try:
+                if self.state_dictionary[seed]:
+                    sentence = seed
+                    for i in range(chain_length):
+                        sentence += ' '
+                        sentence += self.sample_next_state(self.state_dictionary,
+                                                           ' '.join(sentence.split()[i: i+self.state_length]))
+                    return sentence
+            except KeyError:
+                return 'Seed does not exist in Markov chain. Aborting.'
+        else:
+            seed = random.choices(list(self.state_dictionary.keys()))[0]
+            sentence = seed
+            for i in range(chain_length):
+                sentence += ' '
+                sentence += self.sample_next_state(self.state_dictionary,
+                                                   ' '.join(sentence.split()[i: i+self.state_length]))
+        return sentence
+
+
 def clean_input_text(filename: str) -> list[str]:
     text = ""
     with open(filename, 'r') as f:
@@ -12,6 +70,7 @@ def clean_input_text(filename: str) -> list[str]:
     text = text.replace('\t', ' ')
     text = text.replace('“', ' " ')
     text = text.replace('”', ' " ')
+    text = text.replace('_', '')
     for spaced in ['.', '-', ',', '!', '?', '(', '—', ')', ';']:
         text = text.replace(spaced, ' {0} '.format(spaced))
     text_list = text.split(' ')
@@ -19,60 +78,8 @@ def clean_input_text(filename: str) -> list[str]:
     return text_list
 
 
-def create_word_dictionary(word_list: list[str], state_length: int = 2, markov_system: dict[str, dict[str, int]] = None) -> dict[str, dict[str, int]]:
-    k_length_states = [' '.join(word_list[i:i+state_length]) for i, _ in enumerate(word_list[:-state_length])]
-    if not markov_system:
-        markov_system = {k_state: {} for k_state in set(k_length_states)}
-    else:
-        new_markov_system = {k_state: {} for k_state in set(k_length_states)}
-        markov_system = new_markov_system | markov_system
-    for index, k_length_state in enumerate(k_length_states[:-1]):
-        if k_length_states[index+1] in word_dict[k_length_state]:
-            markov_system[k_length_state][k_length_states[index+1].split()[-1]] += 1
-        else:
-            markov_system[k_length_state][k_length_states[index+1].split()[-1]] = 1
-    return markov_system
-
-
-def save_dictionary(markov_system: dict[str, dict[str, int]], filename: str = 'markov_system.json') -> None:
-    with open(filename, 'w') as f:
-        json.dump(markov_system, f)
-
-
-def load_dictionary(filename: str = 'markov_system.json') -> dict[str, dict[str, int]]:
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-
-def sample_next_state(markov_system: dict[str, dict[str, int]], current_state: str):
-    next_state_vector_weights = [weight for weight in markov_system[current_state].values()]
-    next_state_vector = [key for key in markov_system[current_state].keys()]
-    return random.choices(next_state_vector, next_state_vector_weights)[0]
-
-
-def stochastic_chain(markov_system: dict[str, dict[str, int]], seed: str = None, chain_length: int = 15) -> str:
-    k = len(list(markov_system.keys())[0].split())
-    if seed:
-        try:
-            if markov_system[seed]:
-                sentence = seed
-                for i in range(chain_length):
-                    sentence += ' '
-                    sentence += sample_next_state(markov_system, ' '.join(sentence.split()[i: i+k]))
-                return sentence
-        except KeyError:
-            return 'Seed does not exist in Markov chain. Aborting.'
-    else:
-        seed = random.choices(list(markov_system.keys()))[0]
-        sentence = seed
-        for i in range(chain_length):
-            sentence += ' '
-            sentence += sample_next_state(markov_system, ' '.join(sentence.split()[i: i+k]))
-        return sentence
-
-
 def clean_output_text(output_text: str) -> str:
-    if output_text[0] in ['.', '-', ',', '!', '?', '(', '—', ')', '"']:
+    if output_text[0] in ['.', '-', ',', '!', '?', '(', '—', ')', '"', ';']:
         output_text = output_text[1:]
     if output_text[0].islower():
         output_text = output_text[0].upper() + output_text[1:]
@@ -93,7 +100,7 @@ parser.add_argument('-g', '--gen', help='Generate a Markov system from a given t
 parser.add_argument('-k', help='Changes the length of the Markov state to the given number of words.',
                     nargs='?', metavar='state_length', default=2, const=2)
 parser.add_argument('-o', '--output', help='Changes where the file in which the Markov system is stored.',
-                    nargs='?', metavar='output_file', const='markov_system.json')
+                    nargs='?', metavar='output_file', const='markov_system.json', default='markov_system.json')
 parser.add_argument('-a', '--append', nargs='?', help='Add data to an existing Markov system.',
                     metavar='input_filename', const='input.txt')
 parser.add_argument('-l', '--length', metavar='chain_length',
@@ -106,33 +113,27 @@ parser.add_argument('-s', '--seed', nargs='?', help='Initial state of Markov sys
 if __name__ == "__main__":
     args = parser.parse_args()
     args_dict = {key: value for key, value in vars(args).items() if value}
+    markov_system = MarkovSystem(int(args_dict['k']))
     if 'gen' in args_dict.keys():
         try:
-            word_dict = create_word_dictionary(clean_input_text(args_dict['gen']), int(args_dict['k']))
+            markov_system.save_file = args_dict['output']
+            markov_system.load_text(args_dict['gen'])
+            markov_system.save()
         except FileNotFoundError:
             print('File does not exist. Aborting.')
             sys.exit()
         except ValueError:
             print('Invalid state length. Aborting.')
             sys.exit()
-        if 'output' in args_dict.keys():
-            save_dictionary(word_dict, args_dict['output'])
-        else:
-            save_dictionary(word_dict)
     elif 'input' in args_dict.keys():
-        word_dict = load_dictionary(args_dict['input'])
+        markov_system.save_file = args_dict['input']
+        markov_system.load()
         if 'seed' in args_dict.keys():
-            print(clean_output_text(stochastic_chain(word_dict, args_dict['seed'], args_dict['length'])))
+            print(clean_output_text(markov_system.stochastic_chain(args_dict['seed'], args_dict['length'])))
         else:
-            print(clean_output_text(stochastic_chain(word_dict, chain_length=args_dict['length'])))
+            print(clean_output_text(markov_system.stochastic_chain(chain_length=args_dict['length'])))
     elif 'append' in args_dict.keys():
-        if 'output' in args_dict.keys():
-            word_dict = load_dictionary(args_dict['output'])
-        else:
-            word_dict = load_dictionary()
-        word_dict = create_word_dictionary(clean_input_text(
-            args_dict['append']), len(list(word_dict.keys())[0].split()), word_dict)
-        if 'output' in args_dict.keys():
-            save_dictionary(word_dict, args_dict['output'])
-        else:
-            save_dictionary(word_dict)
+        markov_system.save_file = args_dict['output']
+        markov_system.load()
+        markov_system.load_text(args_dict['append'])
+        markov_system.save()
